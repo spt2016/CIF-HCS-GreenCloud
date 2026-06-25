224eb1a6e1426c485cebb0d8403c568a572627d7
Dr. Siba Prasada Tripathy <siba.dilu@gmail.com>
2026-06-25 12:36:29 +0530

    Created HostManager skeleton

---
+++ src/main/java/org/siba/cloud/datacenter/HostManager.java
@@ -0,0 +1,17 @@
+package org.siba.cloud.datacenter;
+
+import org.cloudsimplus.hosts.Host;
+import java.util.List;
+
+public class HostManager {
+
+    public List<Host> createHosts() {
+
+        System.out.println("--------------------------------");
+        System.out.println("Creating Hosts...");
+        System.out.println("--------------------------------");
+
+        return List.of();
+
+    }
+}
