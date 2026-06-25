7d8dfde6d1bc50b517aa8a283fdc38557a2a7575
Dr. Siba Prasada Tripathy <siba.dilu@gmail.com>
2026-06-25 12:11:15 +0530

    Implemented DatacenterManager

--- src/main/java/org/siba/cloud/datacenter/DatacenterManager.java
+++ src/main/java/org/siba/cloud/datacenter/DatacenterManager.java
@@ -1,4 +1,20 @@
 package org.siba.cloud.datacenter;
 
+import org.cloudsimplus.core.CloudSimPlus;
+
 public class DatacenterManager {
+
+    private final CloudSimPlus simulation;
+
+    public DatacenterManager(CloudSimPlus simulation) {
+        this.simulation = simulation;
+    }
+
+    public void createDatacenter() {
+
+        System.out.println("--------------------------------------");
+        System.out.println("Creating Datacenter...");
+        System.out.println("--------------------------------------");
+
+    }
 }
