ed7dda04cf7f1374e6150e7f9e0dcc09cc04b39d
Dr. Siba Prasada Tripathy <siba.dilu@gmail.com>
2026-06-25 12:40:15 +0530

    Implemented physical host creation

--- src/main/java/org/siba/cloud/datacenter/HostManager.java
+++ src/main/java/org/siba/cloud/datacenter/HostManager.java
@@ -1,6 +1,11 @@
 package org.siba.cloud.datacenter;
 
 import org.cloudsimplus.hosts.Host;
+import org.cloudsimplus.hosts.HostSimple;
+import org.cloudsimplus.resources.Pe;
+import org.cloudsimplus.resources.PeSimple;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class HostManager {
@@ -8,10 +13,29 @@ public class HostManager {
     public List<Host> createHosts() {
 
         System.out.println("--------------------------------");
-        System.out.println("Creating Hosts...");
+        System.out.println("Creating Physical Hosts");
         System.out.println("--------------------------------");
 
-        return List.of();
+        List<Host> hostList = new ArrayList<>();
 
+        // Create CPU cores (Processing Elements)
+        List<Pe> peList = new ArrayList<>();
+
+        for(int i=0;i<8;i++){
+            peList.add(new PeSimple(1000));
+        }
+
+        Host host = new HostSimple(
+                16384,      // RAM (MB)
+                100000,     // Bandwidth (Mbps)
+                1000000,    // Storage (MB)
+                peList
+        );
+
+        hostList.add(host);
+
+        System.out.println("Host Created Successfully.");
+
+        return hostList;
     }
 }
