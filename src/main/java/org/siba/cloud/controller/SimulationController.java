ce25e7c1780f760bc0b498cc6ed72b360d287554
Dr. Siba Prasada Tripathy <siba.dilu@gmail.com>
2026-06-29 12:16:10 +0530

    Version 1.0 - Cloud Infrastructure Layer Completed

--- src/main/java/org/siba/cloud/controller/SimulationController.java
+++ src/main/java/org/siba/cloud/controller/SimulationController.java
@@ -1,4 +1,41 @@
 package org.siba.cloud.controller;
 
+import org.cloudsimplus.core.CloudSimPlus;
+import org.siba.cloud.infrastructure.InfrastructureManager;
+
+/**
+ * ---------------------------------------------------------
+ * SimulationController
+ *
+ * Controls the complete execution of the Cloud Simulation.
+ * ---------------------------------------------------------
+ */
 public class SimulationController {
+
+    private final CloudSimPlus simulation;
+
+    public SimulationController() {
+        simulation = new CloudSimPlus();
+    }
+
+    public void startSimulation() {
+
+        System.out.println("\n========================================");
+        System.out.println("CIF-HCS Framework");
+        System.out.println("Green Cloud Computing Simulation");
+        System.out.println("========================================");
+
+        InfrastructureManager infrastructure =
+                new InfrastructureManager(simulation);
+
+        infrastructure.initialize();
+
+        System.out.println("\nStarting CloudSim Simulation...");
+
+        simulation.start();
+
+        System.out.println("\nSimulation Completed Successfully.");
+
+    }
+
 }
